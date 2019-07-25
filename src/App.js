2770026199f707127b3baf1909ddf434afef4b9d
Bogdan <bbogdan2008@yahoo.com>
2019-07-26 01:19:22 +0300

    INT - react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { hot } from "react-hot-loader";
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import "./App.css";
+import Home from './Home.jsx';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1 className="title">Lunch Break ...</h1>
-      </div>
+      <Router>
+        <Route exact path="/" component={Home} />
+      </Router>
     );
   }
 }
